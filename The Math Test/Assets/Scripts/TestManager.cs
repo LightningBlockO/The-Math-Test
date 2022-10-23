7f155c9c86bebabd2d595a1e454644e1a8bdd0f0
bryce-eldridge <bryce.eldridge121003@gmail.com>
2022-10-23 23:07:14 +1100

    Test Script Created

---
+++ The Math Test/Assets/Scripts/TestManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestManager : MonoBehaviour
+{
+    public GameObject[] Levels;
+
+    int currentLevel;
+
+    public void correctAnswer()
+    {
+        if(currentLevel + 1 != Levels.Length)
+        {
+            Levels[currentLevel].SetActive(false);
+
+            currentLevel++;
+            Levels[currentLevel].SetActive(true);
+        }
+    }
+}
